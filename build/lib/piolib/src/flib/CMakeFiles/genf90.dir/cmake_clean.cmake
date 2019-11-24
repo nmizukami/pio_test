34f56ae7e997355318f730aeb24d8d0b6fe1d113
Naoki Mizukami <mizukami@ucar.edu>
2019-11-24 15:51:38 -0700

    remove pio stuff

--- build/lib/piolib/src/flib/CMakeFiles/genf90.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/genf90"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/genf90.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
